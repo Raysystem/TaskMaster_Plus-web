7213acf9a7b9b8dce240c2bc9a7f919c1b0f53bb
Raysystem <rayproducoes7@gmail.com>
2024-04-15 10:29:49 -0300

    feat: coreçao de erros

--- src/functions/authGuard.tsx
+++ src/functions/authGuard.tsx
@@ -3,7 +3,7 @@ import load from "../../public/loading.gif";
 import router from "next/router";
 import Head from "next/head";
 import useAppData from "../data/hook/useAppData";
-export default function AuthGuard(jsx) {
+export default function AuthGuard(jsx:any) {
     const ctx = useAppData()
     function renderCont() {
         return (
